4987840d9569cbec0eba5313dc7a92caeab65472
mayli4 <sydsleeper@gmail.com>
2025-12-04 08:45:43 -0500

    refactor: rename textures to images

--- src/SlugsMod/Common/CommonSlimeModifications.cs
+++ src/SlugsMod/Common/CommonSlimeModifications.cs
@@ -7,22 +7,24 @@ namespace SlugsMod.Common;
 
 [UsedImplicitly]
 internal sealed class CommonSlimeModifications : GlobalNPC {
-    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) {
-        return entity.type 
-            is NPCID.BlueSlime 
-            or NPCID.IceSlime 
-            or NPCID.UmbrellaSlime;
-    }
+    private bool _wasAirborneLastFrame;
     
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type 
+        is NPCID.BlueSlime 
+        or NPCID.IceSlime 
+        or NPCID.UmbrellaSlime;
+
+    public override bool InstancePerEntity => true;
+
     [OnLoad, UsedImplicitly]
     static void ReplaceTextures() {
-        TextureAssets.Npc[NPCID.BlueSlime] = Assets.Textures.NPCs.CommonSlime.Asset;
-        TextureAssets.Npc[NPCID.IceSlime] = Assets.Textures.NPCs.IceSlime.Asset;
-        TextureAssets.Npc[NPCID.UmbrellaSlime] = Assets.Textures.NPCs.UmbrellaSlime.Asset;
+        TextureAssets.Npc[NPCID.BlueSlime] = Assets.Images.NPCs.CommonSlime.Asset;
+        TextureAssets.Npc[NPCID.IceSlime] = Assets.Images.NPCs.IceSlime.Asset;
+        TextureAssets.Npc[NPCID.UmbrellaSlime] = Assets.Images.NPCs.UmbrellaSlime.Asset;
     }
     
     [GlobalNPCHooks.PostAI, UsedImplicitly]
-    static void FaceDirection(NPC npc) {
+    void SlimeEffects(NPC npc) {
         if (npc.velocity.X != 0) {
             npc.spriteDirection = (npc.velocity.X > 0 ? 1 : -1);
         }
@@ -30,5 +32,14 @@ internal sealed class CommonSlimeModifications : GlobalNPC {
         npc.rotation = npc.velocity.X * 0.05f * npc.velocity.Y * -0.5f;
         npc.rotation = MathHelper
             .Clamp(npc.rotation, -0.25f, 0.52f);
+        
+        bool currentlyAirborne = (npc.velocity.Y != 0f && npc.collideY);
+        _wasAirborneLastFrame = currentlyAirborne;
+
+        if(_wasAirborneLastFrame) {
+            for(int i = 0; i < Main.rand.Next(5, 10); i++) {
+                var dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.t_Slime, 0, 0, 0, npc.color);
+            }
+        }
     }
 }
