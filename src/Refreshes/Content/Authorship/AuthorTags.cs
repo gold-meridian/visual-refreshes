75db531c6961da4c258b8dd829baf166f91e15c8
mayli4 <sydsleeper@gmail.com>
2025-12-19 05:30:32 -0500

    feat: authorship

---
+++ src/Refreshes/Content/Authorship/AuthorTags.cs
@@ -0,0 +1,18 @@
+﻿using Daybreak.Common.Features.Authorship;
+using JetBrains.Annotations;
+
+namespace Refreshes.Content;
+
+internal abstract class CommonAuthorTag : AuthorTag
+{
+    private const string suffix = "Tag";
+
+    public override string Name => base.Name.EndsWith(suffix) ? base.Name[..^suffix.Length] : base.Name;
+
+    public override string Texture => string.Join('/', Assets.Images.UI.AuthorTags.Mathica.KEY.Split('/')[..^1]) + '/' + Name;
+}
+
+[UsedImplicitly] internal class MathicaTag : CommonAuthorTag;
+[UsedImplicitly] internal class TobiasTag : CommonAuthorTag;
+[UsedImplicitly] internal class TomatTag : Daybreak.Content.Authorship.TomatTag;
+[UsedImplicitly] internal class BlockarozTag : Daybreak.Content.Authorship.BlockarozTag;
--- src/Refreshes/Impl.cs
+++ src/Refreshes/Impl.cs
@@ -2,11 +2,15 @@
 global using Microsoft.Xna.Framework.Graphics;
 global using Terraria;
 global using Terraria.ModLoader;
+using Daybreak.Common.Features.Authorship;
+using Daybreak.Common.Features.ModPanel;
 
 namespace Refreshes;
 
-partial class ModImpl {
+partial class ModImpl : IHasCustomAuthorMessage  {
     public ModImpl() {
         MusicAutoloadingEnabled = false;
     }
+
+    public string GetAuthorText() => AuthorText.GetAuthorTooltip(this, Mods.Refreshes.UI.ModIcon.AuthorHeader.GetTextValue());
 }
