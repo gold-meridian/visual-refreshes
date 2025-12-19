784270b03d341f3e4d2daeca9510ae4fdc8eb2d5
mayli4 <sydsleeper@gmail.com>
2025-12-19 06:19:23 -0500

    wip: cloud in a bottle stubs for tomorrow

---
+++ src/Refreshes/Content/BottleAccessories/CloudInABottle.cs
@@ -0,0 +1,20 @@
+﻿using Daybreak.Common.Features.Hooks;
+using MonoMod.RuntimeDetour;
+
+namespace Refreshes.Content.BottleAccessories;
+
+internal sealed class CloudInABottleModifications {
+    //cancels out dust trail
+    [ModPlayerHooks.CanShowExtraJumpVisuals]
+    public static bool CancelVanillaVisuals(ExtraJump jump) => jump != ExtraJump.CloudInABottle;
+    
+    private static Hook _hook = null!;
+    private delegate void orig_OnStarted(CloudInABottleJump self, Player player, ref bool playSound);
+
+    //cancels out initial cloud puff
+    [OnLoad]
+    private static void HookCloud() => _hook = new Hook(typeof(CloudInABottleJump).GetMethod("OnStarted")!, (orig_OnStarted _, CloudInABottleJump _, Player _, ref bool _) => { });
+    
+    [OnUnload]
+    private static void Unhook() => _hook.Dispose();
+}
