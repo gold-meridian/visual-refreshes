6c55608799eb1ecfcd3db6c72fb05ed70eb5cc73
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-12-24 01:24:29 +0200

    feat: basic working strip trail

---
+++ src/Refreshes/Assets/Shaders/BasicTrail.fx
@@ -0,0 +1,42 @@
+sampler uImage0 : register(s0);
+
+matrix uTransformMatrix;
+
+struct VertexShaderInput
+{
+    float4 Position : POSITION0;
+    float4 Color : COLOR0;
+    float3 Coord : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : POSITION0;
+    float4 Color : COLOR0;
+    float3 Coord : TEXCOORD0;
+};
+
+VertexShaderOutput VertexShaderFunction(in VertexShaderInput input)
+{
+    VertexShaderOutput output = (VertexShaderOutput) 0;
+    output.Position = mul(input.Position, uTransformMatrix);
+    output.Color = input.Color;
+    output.Coord = input.Coord;
+    return output;
+}
+
+float4 PixelShaderFunction(in VertexShaderOutput input) : COLOR0
+{
+    float2 uv = input.Coord;
+
+    return tex2D(uImage0, uv);
+}
+
+technique Technique1
+{
+    pass BasicTrailPass
+    {
+        VertexShader = compile vs_2_0 VertexShaderFunction();
+        PixelShader = compile ps_2_0 PixelShaderFunction();
+    }
+}
---
+++ src/Refreshes/Content/Yoyos/Terrarian.cs
@@ -0,0 +1,67 @@
+﻿using Daybreak.Common.Features.Hooks;
+using Microsoft.CodeAnalysis;
+using Microsoft.Xna.Framework.Graphics;
+using Refreshes.Common.Rendering;
+using Refreshes.Core;
+using Terraria.GameContent;
+using Terraria.ID;
+
+namespace Refreshes.Content.Yoyos;
+
+internal sealed class TerrarianModifications : GlobalProjectile
+{
+    private Vector2[] previousPositions = new Vector2[20];
+    private float[] previousRotations = new float[20];
+
+    public override bool InstancePerEntity => true;
+
+    public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
+    {
+        return entity.type == ProjectileID.Terrarian;
+    }
+
+    public override void SetDefaults(Projectile entity)
+    {
+        for (int i = 0; i < previousPositions.Length; i++)
+        {
+            previousPositions[i] = entity.position;
+            previousRotations[i] = entity.velocity.ToRotation();
+        }
+    }
+
+    public override bool PreAI(Projectile projectile)
+    {
+        for (int i = previousPositions.Length - 1; i > 0; i--)
+        {
+            previousPositions[i] = previousPositions[i - 1];
+            previousRotations[i] = previousRotations[i - 1];
+        }
+        previousPositions[0] = projectile.position;
+        previousRotations[0] = projectile.velocity.ToRotation();
+
+        return true;
+    }
+
+    public override bool PreDraw(Projectile projectile, ref Color lightColor)
+    {
+        var trailTexture = TextureAssets.MagicPixel.Value;
+
+        var trailShader = Assets.Shaders.BasicTrail.CreateBasicTrailPass();
+        trailShader.Parameters.uTransformMatrix = Main.GameViewMatrix.NormalizedTransformationmatrix;
+        trailShader.Parameters.uImage0 = new HlslSampler { Texture = trailTexture, Sampler = SamplerState.PointClamp };
+        trailShader.Apply();
+
+        static Color StripColorFunction(float p) => Color.White;
+        static float StripWidthFunction(float p) => 3f;
+
+        PrimitiveRenderer.DrawStripPadded(previousPositions, previousRotations, StripColorFunction, StripWidthFunction, projectile.Size / 2 - Main.screenPosition);
+
+        Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+
+        var yoyoTexture = TextureAssets.Projectile[ProjectileID.Terrarian].Value;
+
+        Main.spriteBatch.Draw(yoyoTexture, projectile.position - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, yoyoTexture.Size() * 0.5f, projectile.scale, SpriteEffects.None, 0f);
+
+        return false;
+    }
+}
