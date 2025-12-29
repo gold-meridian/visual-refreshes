d4822ce03c9f93b3d7638ced3769e1399f7ea78b
mayli4 <sydsleeper@gmail.com>
2025-12-29 05:29:52 -0500

    wip: boot accessory stubs

---
+++ src/Refreshes/Content/BootAccessories/RunningBoots.cs
@@ -0,0 +1,18 @@
+﻿using Daybreak.Common.Features.Hooks;
+
+namespace Refreshes.Content.BootAccessories;
+
+internal sealed class RunningBoots
+{
+    [OnLoad]
+    private static void ApplyHooks() 
+    {
+        On_Player.RocketBootVisuals += On_PlayerOnRocketBootVisuals_UseCustomParticles;
+    }
+
+    private static void On_PlayerOnRocketBootVisuals_UseCustomParticles(On_Player.orig_RocketBootVisuals orig, Player self)
+    {
+        var isRocketBoots = self.rocketBoots == 1;
+        var isSpectreBoots = self.rocketBoots == 2;
+    }
+}
